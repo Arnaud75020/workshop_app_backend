4ea2da2ec982b57b494adc958d3e81004474de7e
Connor Todd <connortodd0304@gmail.com>
2020-07-10 16:52:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 module.exports = connection;
