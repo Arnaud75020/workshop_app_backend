d35d9db993f19e83f80ea29128eb2464282b1f82
Connor Todd <connortodd0304@gmail.com>
2020-07-11 14:06:00 +0100

    updated port for caprover deployment

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.DB_PORT;
+const port = process.env.PORT || 5000;
 const connection = require('./config.js');
 
 const notificationRouter = require('./routes/notifications.route');
