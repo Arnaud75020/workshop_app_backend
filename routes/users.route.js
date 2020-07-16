629459339a11787cd876b60bc195dea8d24e094e
amduraes <andremdpereira@gmail.com>
2020-07-16 16:53:57 +0100

    working dev

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,104 +2,120 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../config');
 
-
 //GET ALL USERS http://localhost:5000/users
 
 router.get('/', (req, res) => {
-
-    connection.query('SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id GROUP BY u.id', (err, results) => {
-        if (err) {
-            res.status(500).json({
-              error: err.message,
-              sql: err.sql,
-            });
-          } else {
-            res.json(results);
-          }
-    })
+  connection.query(
+    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id GROUP BY u.id',
+    (err, results) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      } else {
+        res.json(results);
+      }
+    }
+  );
 });
-
 
 //GET ALL ATTENDEES http://localhost:5000/users/attendees
 
-
 router.get('/attendees', (req, res) => {
-
-  connection.query('SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 3 GROUP BY u.id', (err, results) => {
+  connection.query(
+    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 3 GROUP BY u.id',
+    (err, results) => {
       if (err) {
-          res.status(500).json({
-            error: err.message,
-            sql: err.sql,
-          });
-        } else {
-          res.json(results);
-        }
-  })
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      } else {
+        res.json(results);
+      }
+    }
+  );
 });
-
 
 //GET ALL SPEAKERS http://localhost:5000/users/speakers
 
-
 router.get('/speakers', (req, res) => {
-
-  connection.query('SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 2 GROUP BY u.id', (err, results) => {
+  connection.query(
+    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 2 GROUP BY u.id',
+    (err, results) => {
       if (err) {
-          res.status(500).json({
-            error: err.message,
-            sql: err.sql,
-          });
-        } else {
-          res.json(results);
-        }
-  })
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      } else {
+        res.json(results);
+      }
+    }
+  );
 });
-
 
 //GET ONE USER http://localhost:5000/:id
 
-
 router.get('/getuser/:id', (req, res) => {
+  const id = req.params.id;
 
-  const id = req.params.id
-
-  connection.query('SELECT u.*, r.role FROM user u JOIN role r ON u.role_id=r.id WHERE u.id= ?',[id], (err, results) => {
+  connection.query(
+    'SELECT u.*, r.role FROM user u JOIN role r ON u.role_id=r.id WHERE u.id= ?',
+    [id],
+    (err, results) => {
       if (err) {
-          res.status(500).json({
-            error: err.message,
-            sql: err.sql,
-          });
-        } else {
-          res.json(results);
-        }
-  })
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      } else {
+        res.json(results);
+      }
+    }
+  );
 });
-
 
 //DELETE ONE USER http://localhost:5000/users/:id
 
-
 router.delete('/:id', (req, res) => {
-
   const id = req.params.id;
 
   connection.query('DELETE FROM user WHERE id = ?', [id], (err, results) => {
-      if(err) {
-          return res.status(500).json({
-                   error: err.message,
-                   sql: err.sql,
-                  });
-      }
-      if(results.affectedRows === 0){
-        return res
-        .status(404)
-        .json({msg: 'user does not exist'})
-      }
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    }
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ msg: 'user does not exist' });
+    }
 
-      return res
-      .status(201)
-      .json(results)
-  })
+    return res.status(201).json(results);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const formData = req.body;
+  const idUpdatedUser = req.params.id;
+  console.log(formData, idUpdatedUser);
+  return connection.query(
+    'UPDATE user SET ? WHERE id = ?',
+    [formData, idUpdatedUser],
+    (err, results) => {
+      if (err) {
+        console.log('ERR', err);
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      res.status(200).send(results);
+      console.log('RESULTS', results);
+    }
+  );
 });
 
 module.exports = router;
