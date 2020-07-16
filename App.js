bde9b3db5949935c58c0c3962cc9337b704c72b0
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-07-16 14:45:32 +0100

    cleaned app.js

--- App.js
+++ App.js
@@ -3,39 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-<<<<<<< HEAD:App.js
-var cors = require('cors')
+const cors = require('cors')
 const port = process.env.PORT || 5000;
-const connection = require('./config.js');
-=======
-const port = process.env.PORT;
 
 const morgan = require('morgan');
 
-const cors = require('cors');
-
 const cookieParser = require('cookie-parser');
->>>>>>> ceab954872c2fc75f460d5a7823e7268a6d1795c:Back-end/App.js
 
 const notificationRouter = require('./routes/notifications.route');
 const userRouter = require('./routes/users.route');
 const workshopRouter = require('./routes/workshop.route');
 const authRouter = require('./routes/auth.route');
 
-<<<<<<< HEAD:App.js
-
-process.on('unhandledRejection', error => {
-  console.error('unhandledRejection', JSON.stringify(error), error.stack);
-  process.exit(1);
-});
-process.on('uncaughtException', error => {
-=======
 process.on('unhandledRejection', (error) => {
   console.error('unhandledRejection', JSON.stringify(error), error.stack);
   process.exit(1);
 });
 process.on('uncaughtException', (error) => {
->>>>>>> ceab954872c2fc75f460d5a7823e7268a6d1795c:Back-end/App.js
   console.log('uncaughtException', JSON.stringify(error), error.stack);
   process.exit(1);
 });
@@ -44,13 +28,6 @@ process.on('beforeExit', () => {
     if (err) console.error(JSON.stringify(err), err.stack);
   });
 });
-<<<<<<< HEAD:App.js
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-=======
->>>>>>> ceab954872c2fc75f460d5a7823e7268a6d1795c:Back-end/App.js
 
 process.on('SIGINT', function () {
   db.stop(function (err) {
