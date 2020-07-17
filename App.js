3c6a81a94367af3b40ab6e65b42ac1d55430fed6
François ARBOGAST <arbogast.francois@gmail.com>
2020-07-17 11:26:20 +0100

    .then + routes updated

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const port = process.env.PORT;
+const bodyParser = require("body-parser");
+const port = process.env.DB_PORT;
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-const notificationRouter = require('./routes/notifications.route');
-const userRouter = require('./routes/users.route');
-const workshopRouter = require('./routes/workshop.route');
-const authRouter = require('./routes/auth.route');
+const notificationRouter = require("./routes/notifications.route");
+const userRouter = require("./routes/users.route");
+const workshopRouter = require("./routes/workshop.route");
+const authRouter = require("./routes/auth.route");
 
-process.on('unhandledRejection', (error) => {
-  console.error('unhandledRejection', JSON.stringify(error), error.stack);
+process.on("unhandledRejection", (error) => {
+  console.error("unhandledRejection", JSON.stringify(error), error.stack);
   process.exit(1);
 });
-process.on('uncaughtException', (error) => {
-  console.log('uncaughtException', JSON.stringify(error), error.stack);
+process.on("uncaughtException", (error) => {
+  console.log("uncaughtException", JSON.stringify(error), error.stack);
   process.exit(1);
 });
-process.on('beforeExit', () => {
+process.on("beforeExit", () => {
   app.close((err) => {
     if (err) console.error(JSON.stringify(err), err.stack);
   });
 });
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function () {
   db.stop(function (err) {
     process.exit(err ? 1 : 0);
   });
@@ -40,12 +40,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/notifications', notificationRouter);
-app.use('/users', userRouter);
-app.use('/workshops', workshopRouter);
-app.use('/auth', authRouter);
+app.use("/notifications", notificationRouter);
+app.use("/users", userRouter);
+app.use("/workshops", workshopRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, (err) => {
   if (err) {
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const connection = require('../config');
+const connection = require("../config");
+const bcrypt = require("bcrypt");
 
 //GET ALL USERS http://localhost:5000/users
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  console.log("users");
   connection.query(
-    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id GROUP BY u.id',
+    "SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id GROUP BY u.id",
     (err, results) => {
       if (err) {
         res.status(500).json({
@@ -22,9 +24,9 @@ router.get('/', (req, res) => {
 
 //GET ALL ATTENDEES http://localhost:5000/users/attendees
 
-router.get('/attendees', (req, res) => {
+router.get("/attendees", (req, res) => {
   connection.query(
-    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 3 GROUP BY u.id',
+    "SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 3 GROUP BY u.id",
     (err, results) => {
       if (err) {
         res.status(500).json({
@@ -40,9 +42,9 @@ router.get('/attendees', (req, res) => {
 
 //GET ALL SPEAKERS http://localhost:5000/users/speakers
 
-router.get('/speakers', (req, res) => {
+router.get("/speakers", (req, res) => {
   connection.query(
-    'SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 2 GROUP BY u.id',
+    "SELECT u.*, r.role, COUNT(u_w.user_id) AS workshop_count FROM user u JOIN role r ON u.role_id=r.id LEFT JOIN user_workshops u_w ON u.id=u_w.user_id  WHERE role_id = 2 GROUP BY u.id",
     (err, results) => {
       if (err) {
         res.status(500).json({
@@ -58,11 +60,11 @@ router.get('/speakers', (req, res) => {
 
 //GET ONE USER http://localhost:5000/:id
 
-router.get('/getuser/:id', (req, res) => {
+router.get("/getuser/:id", (req, res) => {
   const id = req.params.id;
 
   connection.query(
-    'SELECT u.*, r.role FROM user u JOIN role r ON u.role_id=r.id WHERE u.id= ?',
+    "SELECT u.*, r.role FROM user u JOIN role r ON u.role_id=r.id WHERE u.id= ?",
     [id],
     (err, results) => {
       if (err) {
@@ -79,10 +81,10 @@ router.get('/getuser/:id', (req, res) => {
 
 //DELETE ONE USER http://localhost:5000/users/:id
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const id = req.params.id;
 
-  connection.query('DELETE FROM user WHERE id = ?', [id], (err, results) => {
+  connection.query("DELETE FROM user WHERE id = ?", [id], (err, results) => {
     if (err) {
       return res.status(500).json({
         error: err.message,
@@ -90,30 +92,56 @@ router.delete('/:id', (req, res) => {
       });
     }
     if (results.affectedRows === 0) {
-      return res.status(404).json({ msg: 'user does not exist' });
+      return res.status(404).json({ msg: "user does not exist" });
     }
 
     return res.status(201).json(results);
   });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/change-password", (req, res) => {
+  const formData = req.body;
+
+  const { email, newPassword } = formData;
+  console.log(formData);
+
+  bcrypt.hash(newPassword, 10, (err, hash) => {
+    return connection.query(
+      "UPDATE user SET password = ? WHERE email = ?",
+      [hash, email],
+      (err, results) => {
+        if (err) {
+          console.log("ERR", err);
+          return res.status(500).json({
+            error: err.message,
+            sql: err.sql,
+          });
+        }
+        res.status(200).send(results);
+        console.log("RESULTS", results);
+      }
+    );
+  });
+});
+
+router.put("/:id", (req, res) => {
   const formData = req.body;
   const idUpdatedUser = req.params.id;
   console.log(formData, idUpdatedUser);
+
   return connection.query(
-    'UPDATE user SET ? WHERE id = ?',
+    "UPDATE user SET ? WHERE id = ?",
     [formData, idUpdatedUser],
     (err, results) => {
       if (err) {
-        console.log('ERR', err);
+        console.log("ERR", err);
         return res.status(500).json({
           error: err.message,
           sql: err.sql,
         });
       }
       res.status(200).send(results);
-      console.log('RESULTS', results);
+      console.log("RESULTS", results);
     }
   );
 });
