d518a5dbfd112d3aa811f8af32e84535253cc9fa
amduraes <andremdpereira@gmail.com>
2020-07-21 11:54:06 +0100

    uncommented

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -93,7 +93,7 @@ router.post('/login', function (req, res) {
     const token = jwt.sign({ user }, process.env.JWT_SECRET);
     res.cookie('_p_c', token, {
       expires: new Date(Date.now() + 44600000),
-      // secure: true,
+      secure: true,
       httpOnly: true,
       sameSite: 'strict',
     });
